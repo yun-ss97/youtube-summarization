ceeed197a47f3613b2bf875b4af9c6a8b616a8dc
ys <jasmin971108@gmail.com>
2021-03-23 20:10:06 +0900

    Add inference.py

--- inference.py
+++ inference.py
@@ -3,6 +3,14 @@ import json
 from pororo import Pororo
 import IPython
 
+def make_summary(data, summary_type, video_name):
+    summary = Pororo(task = 'summarization', model = summary_type, lang = 'ko')
+    result = summary(data)
+    text = open('output/[{}]{}.txt'.format(summary_type[:3],video_name), 'w')
+    text.write(result)
+    text.close()
+
+
 label_path = 'data/label'
 
 label_list = os.listdir('data/label')
@@ -12,10 +20,4 @@ for i in range(len(label_list)):
         label_json = json.load(f)
 
     print('Making Summary:', label_list[i])
-    summary = Pororo(task = 'summarization', model = 'abstractive', lang = 'ko')
-
-    sum_result = summary(label_json['text'])
-
-    text = open('output/{}.txt'.format(label_list[i]), 'w')
-    text.write(sum_result)
-    text.close()
+    make_summary(label_json['text'], 'abstractive', label_list[i])
