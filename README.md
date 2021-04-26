716b29845971e944eac4d7a8f33ffe20a11a2687
ys <jasmin971108@gmail.com>
2021-04-26 15:02:10 +0900

    Add code to extract script

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# youtube-summarization
+## youtube-summarization
+
+### [Step1] Download Youtube Video to Local
+
+```python
+python download_video.py --video_list_path {} --video_path {}
+```
+
+### [Step2] Extract video script via Clova Speech API
+
+```python
+python clovaspeech_script.py --video_path {} --label_path {}
+```
--- download_video.py
+++ download_video.py
@@ -1,9 +1,19 @@
 ## python -m pip install pytube
 
 from pytube import YouTube
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--video_list_path', default = 'video_list.txt')
+parser.add_argument('--video_path', default='./data/video')
+
+args = parser.parse_args()
+
+
 
 # video list to extract
-video_list = open('video_list.txt','r')
+video_list = open(args.video_list_path,'r')
 
 while True:
     line = video_list.readline()
@@ -12,8 +22,8 @@ while True:
     ## youtube homepage link
     yt = YouTube(line)
     yt.streams.filter(progressive=True, 
-        file_extension= 'mp4').order_by('resolution').desc().first().download('./data/video')
+        file_extension= 'mp4').order_by('resolution').desc().first().download(args.video_path)
 
-    print('Audio Extracted: {}'.format(line))
+    print('[Video Downloaded] {}'.format(line))
 
 video_list.close()
